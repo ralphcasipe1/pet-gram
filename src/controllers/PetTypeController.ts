6736ff3b17e8bb29b989a5939a18d88c7a805832
Ralph Kevin Casipe <ralphcasipe1@gmail.com>
2018-11-05 16:31:25 +0800

    Update Schema and change it to asyc/await

--- src/controllers/PetTypeController.ts
+++ src/controllers/PetTypeController.ts
@@ -2,59 +2,41 @@ import { Request, Response } from 'express'
 import PetType from '../models/PetType'
 
 export default class PetTypeController {
-  public index(_, response: Response) {
-    return PetType.find({}, (error: Error, petTypes) => {
-      if (error) {
-        response.send(error)
-      }
-      response.send(petTypes)
-    })
+  public async index(_, response: Response) {
+    const petTypes = await PetType.find()
+    
+    return response.send(petTypes)
   }
 
-  public show(request: Request, response: Response) {
-    return PetType.findById(
+  public async show(request: Request, response: Response) {
+    const petType = await PetType.findById(
       request.params.petTypeId, 
-      (error: Error, petType) => {
-        if (error) {
-          response.send(error)
-        }
-        response.send(petType)
-      })
+    )
+    
+    return response.send(petType)
   }
 
-  public create(request: Request, response: Response) {
-    return new PetType(request.body).save((error: Error, petType) => {
-      if (error) {
-        response.send(error)
-      }
-
-      response.send(petType)
-    })
+  public async create(request: Request, response: Response) {
+    const petType = await new PetType(request.body).save()
+    
+    return response.send(petType)
   }
 
-  public update(request: Request, response: Response) {
-    return PetType.findOneAndUpdate(
+  public async update(request: Request, response: Response) {
+    const petType = await PetType.findOneAndUpdate(
       { _id: request.params.petTypeId },
       request.body,
       { new: true },
-      (error: Error, petType) => {
-        if (error) {
-          response.send(error)
-        }
-        response.send(petType)
-      }
     )
+
+    return response.send(petType)
   }
 
-  public destroy(request: Request, response: Response) {
-    return PetType.deleteOne(
-      { _id: request.params.petTypeId },
-      (error: Error) => {
-        if (error) {
-          response.send(error)
-        }
-        response.send({ message: 'Successfully deleted!' })
-      }
+  public async destroy(request: Request, response: Response) {
+    await PetType.deleteOne(
+      { _id: request.params.petTypeId }
     )
+
+    return response.send({ message: 'Successfully deleted!' })
   }
 }
--- src/models/PetType.ts
+++ src/models/PetType.ts
@@ -3,6 +3,9 @@ import * as mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const PetTypeSchema = new Schema({
+  pets: [{
+    pet: { type: Schema.Types.ObjectId, ref: 'Pet' },
+  }],
   name: {
     type: String,
     required: 'Enter a name'
